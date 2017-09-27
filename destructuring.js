2ebd88d84c1128482ed32cfd6496c42dd63a9f7f
Brant Bagnall <brant.bagnall@gmail.com>
2017-09-27 14:38:01 -0600

    did first two problems as required

--- destructuring.js
+++ destructuring.js
@@ -2,26 +2,35 @@
 // DESTRUCTURING
 // ========================
 
-// Use object destructuring to save the property values from the object carDetails into new variables. 
+// Use object destructuring to save the property values from the object carDetails into new variables.
 
 var carDetails = {
   color: 'red',
   make: 'toyota',
   model: 'tacoma',
   year: 1994
-}
+};
 
 // CODE HERE
-
+var {
+  color,
+  make,
+  model,
+  year
+} = carDetails;
 
 // ========================
 
 
 // In the function below named greeting, it is receiving an object as a parameter. Use object destructuring to save the object properties to new variables. The property names are firstName, lastName, and title. Return the concatenated string.
 
-function greeting( obj ) {
+function greeting(obj) {
   // CODE HERE
-  
+  var {
+    title,
+    firstName,
+    lastName
+  } = obj;
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
 }
 
@@ -31,15 +40,15 @@ function greeting( obj ) {
 
 // Write a function called totalPopulation that will take in an object. That object will have 4 properties named utah, california, texas and arizona. The property values will be numbers. Use object destructuring to save the property values to new variables. Sum up the values and return the total number.
 
-  // CODE HERE
+// CODE HERE
 
 
 // ========================
 
 
-// Write a function called ingredients that will take in an object. This object will have 3 properties named carb, fat, and protein. The property values will be strings. Use object destructuring to save the property values to new variables. Push these new variables to an array and return the array. 
+// Write a function called ingredients that will take in an object. This object will have 3 properties named carb, fat, and protein. The property values will be strings. Use object destructuring to save the property values to new variables. Push these new variables to an array and return the array.
 
-  // CODE HERE
+// CODE HERE
 
 
 // ========================
@@ -53,12 +62,12 @@ function greeting( obj ) {
 
 // Write a function called largeNumbers that will take a destructured object as it's parameter. The object properties will be named first, second, and third and their values will be numbers. Find the smallest number of the three and return that number.
 
-  // CODE HERE
+// CODE HERE
 
 
 // ========================
 
 
-// Write a function called numberGroups that will take a destructured object as it's parameter. The object properties will be named a, b, and c and their values will be arrays of numbers. Find the longest array and return that array. 
+// Write a function called numberGroups that will take a destructured object as it's parameter. The object properties will be named a, b, and c and their values will be arrays of numbers. Find the longest array and return that array.
 
-  // CODE HERE
+// CODE HERE
